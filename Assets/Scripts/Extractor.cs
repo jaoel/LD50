f32ddf32cb490d1dc77be1ab55e8748014cb354a
Jaoel <josv.svensson@gmail.com>
2022-04-03 09:38:52 +0200

    Add extractor

---
+++ Assets/Scripts/Extractor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+class Extractor : MonoBehaviour {
+    [SerializeField]
+    private Animator _animator = null;
+
+    [SerializeField]
+    private int _maxHealth = 1000;
+
+    public void Start() {
+        
+    }
+
+    private void Update() {
+        if (_maxHealth <= 0) {
+            Debug.LogError("YOU LOST!");
+        }
+    }
+
+    public void BeginExtraction() {
+        _animator.SetBool("active", true);
+    }
+
+    public void EndExtraction() {
+        _animator.SetBool("active", false);
+    }
+}
