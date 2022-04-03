da4c34940701af1784c4533b9566dec9de891200
Jaoel <josv.svensson@gmail.com>
2022-04-03 17:53:18 +0200

    More sound

--- Assets/Scripts/ExtractorAnimationEvents.cs
+++ Assets/Scripts/ExtractorAnimationEvents.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class ExtractorAnimationEvents : MonoBehaviour {
     private float _maxDistToPlayer = 50f;
+    public AudioSource audioSourceThud1;
+    public AudioSource audioSourceThud2;
 
     private Player _player;
 
@@ -15,9 +17,13 @@ public class ExtractorAnimationEvents : MonoBehaviour {
     public void Shake1() {
 
         Shaker.Shake(0.25f * ShakeAmount, 25f * ShakeAmount);
+        audioSourceThud1.pitch = Random.Range(0.95f, 1.05f);
+        audioSourceThud1.Play();
     }
 
     public void Shake2() {
         Shaker.Shake(0.15f * ShakeAmount, 15f * ShakeAmount);
+        audioSourceThud2.pitch = Random.Range(0.95f, 1.05f);
+        audioSourceThud2.Play();
     }
 }
