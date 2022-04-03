ae818a14fe113531e6a7e5c927c1cb2ff3c76ae2
Jaoel <josv.svensson@gmail.com>
2022-04-03 18:29:19 +0200

    Tweak camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 offset = new Vector3(4f, 8f, -6f);
+
+    private void Update() {
+        if (Player.Instance != null) {
+            transform.position = Vector3.Lerp(transform.position, Player.Instance.transform.position + offset, 0.05f);
+            transform.rotation = Quaternion.LookRotation(-offset, Vector3.up);
+        }        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,7 +39,7 @@ public class Player : MonoBehaviour {
     private float _invulnTimer = 0.0f;
     private int _health = 0;
 
-    private float _attackCooldown = .2f;
+    private float _attackCooldown = .1f;
     private float _attackTime = 0f;
 
     private PlayerState _currentState = PlayerState.Moving;
@@ -204,7 +204,7 @@ public class Player : MonoBehaviour {
         Debug.DrawLine(transform.position, transform.position + _targetRotationVector);
         if (_targetRotationVector != Vector3.zero) {
             Quaternion targetRotation = Quaternion.LookRotation(_targetRotationVector, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 7.5f);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Mathf.Clamp(Mathf.Abs(Quaternion.Angle(targetRotation, transform.rotation) * 0.1f), 2f, 45f));
         }
     }
 
